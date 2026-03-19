a08c005d6c7d4bbc68d42c17e461e0d67934d111
Vitor <vitorabcosta@gmail.com>
2026-03-18 23:31:37 -0300

    Ajuste de erros

--- script.js
+++ script.js
@@ -27,10 +27,10 @@ window.login = async function () {
 // 🔄 CONTROLE DE LOGIN
 onAuthStateChanged(window.auth, user => {
   if (user) {
-    document.getElementById("login").style.display = "none";
+    document.getElementById("login-container").style.display = "none";
     document.getElementById("app").style.display = "block";
   } else {
-    document.getElementById("login").style.display = "block";
+    document.getElementById("login-container").style.display = "block";
     document.getElementById("app").style.display = "none";
   }
 });
